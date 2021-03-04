c24cbf7f1588dacd7b9a1c178a32ed40ef71b37a
Yates101 <alecyates94@gmail.com>
2021-03-04 22:25:44 +0000

    instance variables commit

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
 class DockingStation
+
+  attr_reader :bike
+
   def release_bike
-    Bike.new
+    fail 'No bikes left' unless @bike
+    @bike
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
 end
 
 class Bike
--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -5,8 +5,28 @@ describe DockingStation do
     expect(subject).to respond_to(:release_bike)
   end
 
-  it 'release bike creates a bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
+
+  it 'docking station accepts bike as argument to dock' do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it 'stores bike when docked' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes to release' do
+      expect { subject.release_bike }.to raise_error 'No bikes left' 
+    end
+  end
+
 end
